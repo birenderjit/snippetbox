5a37f642c03e21b57c23f5f448a36ef0638f142a
Birenderjit Madan <birenderjit@gmail.com>
2025-05-17 14:34:03 -0500

    slog, json logging and some options for logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,7 +11,11 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	//logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
 
 	mux := http.NewServeMux()
 
