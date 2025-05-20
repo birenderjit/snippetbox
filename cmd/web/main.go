28016fde068430be164f710e0c15174a236fdbe6
Birenderjit Madan <birenderjit@gmail.com>
2025-05-20 08:18:55 -0500

    feat: add a error log handler for http.struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -76,6 +76,12 @@ func main() {
 	srv := &http.Server{
 		Addr:    *addr,
 		Handler: app.routes(),
+
+		// Create a *log.Logger from our structured logger handler, which writes
+		// log entries at Error level, and assign it to the ErrorLog field. If
+		// you would prefer to log the server errors at Warn level instead, you
+		// could pass slog.LevelWarn as the final parameter.
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
