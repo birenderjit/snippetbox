2c0dbda530664ae2bc306067b32183d0d113196b
Birenderjit Madan <birenderjit@gmail.com>
2025-05-20 10:38:40 -0500

    feat: add changes to run a tls http server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -71,6 +72,14 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	// Initialize a tls.Config struct to hold the non-default TLS settings we
+	// want the server to use. In this case the only thing that we're changing
+	// is the curve preferences value, so that only elliptic curves with
+	// assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before.
 	srv := &http.Server{
@@ -82,12 +91,15 @@ func main() {
 		// you would prefer to log the server errors at Warn level instead, you
 		// could pass slog.LevelWarn as the final parameter.
 		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+
+		TLSConfig: tlsConfig,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
 
-	// Call the ListenAndServe() method on our new http.Server struct to start the server.
-	err = srv.ListenAndServe()
+	// Use the ListenAndServeTLS() method to start the HTTPS server.
+	// We pass in the paths to the TLS certificate and corresponding private key as the two parameters.
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
