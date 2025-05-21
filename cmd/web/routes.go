60021dc88ae36c68e33583822085f599b1d46365
Birenderjit Madan <birenderjit@gmail.com>
2025-05-20 23:11:29 -0500

    refactor: user authentication to r.context

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,7 +20,8 @@ func (app *application) routes() http.Handler {
 
 	// Unprotected application routes using the "dynamic" middleware chain.
 	// Use the nosurf middleware on all our 'dynamic' routes.
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	// Add the authenticate() middleware to the chain.
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
